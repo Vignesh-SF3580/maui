55f6ed102a4d98f1ea1d4eea65687715e0f47c45
Vignesh-SF3580 <102575140+Vignesh-SF3580@users.noreply.github.com>
2025-12-23 13:58:02 +0530

    Update Issue25599_1.cs

--- src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/Issue25599_1.cs
+++ src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/Issue25599_1.cs
@@ -3,27 +3,26 @@ using NUnit.Framework;
 using UITest.Appium;
 using UITest.Core;
 
-namespace Microsoft.Maui.TestCases.Tests.Issues
-{
-	public class Issue25599_1 : _IssuesUITest
-	{
-		public Issue25599_1(TestDevice testDevice) : base(testDevice)
-		{
-		}
-		public override string Issue => "Shell Navigating event shows identical Current and Target on tab click";
+namespace Microsoft.Maui.TestCases.Tests.Issues;
 
-		[Test]
-		[Category(UITestCategories.Shell)]
-		public void VerifyNavigatingEventOnTabReselection()
-		{
-			App.WaitForElement("PushButton");
-			App.Tap("PushButton");
-			App.WaitForElement("PopButton");
-			App.TapTab("Home");
-			var currentLabel = App.WaitForElement("MainPageNavigatingCurrentLabel");
-			var targetLabel = App.WaitForElement("MainPageNavigatingTargetLabel");
-			Assert.That(currentLabel.GetText(), Is.Not.EqualTo(targetLabel.GetText()));
-		}
+public class Issue25599_1 : _IssuesUITest
+{
+	public Issue25599_1(TestDevice testDevice) : base(testDevice)
+	{
+	}
+	public override string Issue => "Shell Navigating event shows identical Current and Target on tab click";
+
+	[Test]
+	[Category(UITestCategories.Shell)]
+	public void VerifyNavigatingEventOnTabReselection()
+	{
+		App.WaitForElement("PushButton");
+		App.Tap("PushButton");
+		App.WaitForElement("PopButton");
+		App.TapTab("Home");
+		var currentLabel = App.WaitForElement("MainPageNavigatingCurrentLabel");
+		var targetLabel = App.WaitForElement("MainPageNavigatingTargetLabel");
+		Assert.That(currentLabel.GetText(), Is.Not.EqualTo(targetLabel.GetText()));
 	}
 }
 #endif
