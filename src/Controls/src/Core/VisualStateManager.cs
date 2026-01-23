e0a8528fce5ab9a9dabb050ad6aefc56692a2191
Vignesh-SF3580 <102575140+Vignesh-SF3580@users.noreply.github.com>
2026-01-23 13:35:40 +0530

    removed the changes

--- src/Controls/src/Core/VisualStateManager.cs
+++ src/Controls/src/Core/VisualStateManager.cs
@@ -95,10 +95,7 @@ namespace Microsoft.Maui.Controls
 			var vsgSpecificity = vsgSpecificityValue.Key;
 			groups.Specificity = vsgSpecificity;
 
-			// Use Fromhandler specificity for VSM setters to ensure they override other setters
-			// This is the highest specificity level, ensuring VSM state changes take effect
-			// When UnApply is called, the original style value will be present underneath
-			var specificity = SetterSpecificity.FromHandler;
+			var specificity = vsgSpecificity.CopyStyle(1, 0, 0, 0);
 
 			foreach (VisualStateGroup group in groups)
 			{
